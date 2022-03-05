6e97dc49a5b02e4bb7fd71740eb805c761d6d099
f0x4 <70147253+f0x4@users.noreply.github.com>
2022-03-05 21:07:02 +0300

    Sanity CMS implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+const Home = ({ posts }: Props) => {
   return (
     <div className="">
       <Head>
@@ -13,22 +18,67 @@ const Home: NextPage = () => {
 
       <Header />
 
-      <div className="flex min-h-[400px] items-center justify-between border-y border-black bg-blue-200 bg-[url('/banner.png')] bg-[length:385px_341px] bg-[bottom_1px_right_32px] bg-no-repeat py-10 lg:py-0">
+      <section className="flex min-h-[400px] items-center justify-between border-y border-black bg-blue-200 bg-[url('/banner.png')] bg-[length:385px_341px] bg-[bottom_1px_right_32px] bg-no-repeat py-10 lg:py-0">
         <div className="space-y-5 px-10">
           <h1 className="max-w-xl font-serif text-6xl">
             <span className="underline decoration-black decoration-4">
               Medium
             </span>{' '}
-            is a place to weite, read, and connect.
+            is a place to write, read, and connect.
           </h1>
           <h2>
             It's easy and free to post your thinking on any topic and connect
             with millions of readers.
           </h2>
         </div>
-      </div>
+      </section>
+
+      <section className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post: Post) => (
+          <Link href={`/post/${post.slug.current}`} key={post._id}>
+            <div className="cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </section>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return { props: { posts } }
+}
